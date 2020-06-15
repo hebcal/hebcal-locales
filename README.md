db079591d200038c9df00ce52d16348288476576
Michael J. Radwin <michael@radwin.org>
2020-06-14 22:39:14 -0700

    Use hebcal.registerLocale() from @hebcal/core 1.1.0

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # hebcal-locales
 Translations for Hebcal events in multiple languages
+
+Adds support for the following locales:
+
+* `ashkenazi_litvish`
+* `ashkenazi_poylish`
+* `ashkenazi_standard`
+* `fi`
+* `fr`
+* `hu`
+* `pl`
+* `ru`
+
+Note that `@hebcal/core` has built-in support for `he` and `ashkenazi` locales.
+This package is needed only if you need other locales.
+
+## Installation
+```bash
+$ npm install @hebcal/locales
+```
+
+## Synopsis
+```javascript
+import {hebcal} from '@hebcal/core';
+import '@hebcal/locales';
+
+const options = {
+  year: 2020,
+  sedrot: true,
+  locale: 'ru',
+};
+const events = hebcal.hebrewCalendar(options);
+```
--- src/locale.js
+++ src/locale.js
@@ -1,4 +1,5 @@
 /* eslint-disable camelcase */
+import {hebcal} from '@hebcal/core';
 import po_fi from './fi.po.json';
 import po_fr from './fr.po.json';
 import po_hu from './hu.po.json';
@@ -8,29 +9,11 @@ import po_ashkenazi_litvish from './ashkenazi_litvish.po.json';
 import po_ashkenazi_poylish from './ashkenazi_poylish.po.json';
 import po_ashkenazi_standard from './ashkenazi_standard.po.json';
 
-export const emptyPoData = {
-  headers: {'plural-forms': 'nplurals=2; plural=(n!=1);'},
-  contexts: {'': {}},
-};
-
-/**
- * Hack for bundling instead of dynamic require
- * @param {string} locale
- * @return {Object}
- */
-export function getTranslationObj(locale) {
-  switch (locale) {
-    case 'fi': return po_fi;
-    case 'fr': return po_fr;
-    // case 'he': return po_he;
-    case 'hu': return po_hu;
-    case 'pl': return po_pl;
-    case 'ru': return po_ru;
-    // case 'ashkenazi': return po_ashkenazi;
-    case 'ashkenazi_litvish': return po_ashkenazi_litvish;
-    case 'ashkenazi_poylish': return po_ashkenazi_poylish;
-    case 'ashkenazi_standard': return po_ashkenazi_standard;
-    default:
-      return emptyPoData;
-  }
-}
+hebcal.registerLocale('fi', po_fi);
+hebcal.registerLocale('fr', po_fr);
+hebcal.registerLocale('hu', po_hu);
+hebcal.registerLocale('pl', po_pl);
+hebcal.registerLocale('ru', po_ru);
+hebcal.registerLocale('ashkenazi_litvish', po_ashkenazi_litvish);
+hebcal.registerLocale('ashkenazi_poylish', po_ashkenazi_poylish);
+hebcal.registerLocale('ashkenazi_standard', po_ashkenazi_standard);
