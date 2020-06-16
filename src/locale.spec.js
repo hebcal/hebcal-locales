6ffc8f6e92a361f4bec28885291d10ecb5bd22a5
Michael J. Radwin <michael@radwin.org>
2020-06-16 08:26:42 -0700

    Add a unit test

---
+++ src/locale.spec.js
@@ -0,0 +1,33 @@
+import test from 'ava';
+import {hebcal} from '@hebcal/core';
+import './locale';
+
+test('locale-ru', (t) => {
+  const options = {year: 2020, month: 4, locale: 'ru'};
+  const ev = hebcal.hebrewCalendar(options)[0];
+  t.is(ev.render(), 'Большой Шаббат');
+});
+
+test('locale-ru-ordinal', (t) => {
+  // test numeraljs ordinal
+  const options = {year: 2020, noHolidays: true, omer: true, locale: 'ru'};
+  const ev = hebcal.hebrewCalendar(options)[0];
+  t.is(ev.render(), '1. day of the Omer');
+});
+
+test('addHebrewDates-locale', (t) => {
+  const options = {
+    year: 2017,
+    month: 3,
+    noHolidays: true,
+    addHebrewDates: true,
+    locale: 'fr',
+  };
+  const evFR = hebcal.hebrewCalendar(options)[0];
+  t.is(evFR.getDesc(), '3 Adar 5777');
+  t.is(evFR.render(), '3e Adar, 5777');
+  options.locale = 'ru';
+  const evRU = hebcal.hebrewCalendar(options)[0];
+  t.is(evRU.getDesc(), '3 Adar 5777');
+  t.is(evRU.render(), '3. Адар, 5777');
+});
