e5c5e3cc0e232d38b560d984dae639f6ce6dfb11
Michael J. Radwin <michael@radwin.org>
2022-05-02 10:30:25 -0700

    Prefer const in .mjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import {nodeResolve} from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
 import pkg from './package.json';
@@ -11,7 +10,6 @@ export default [
     input: 'src/locale.js',
     output: [
       {file: pkg.main, format: 'cjs', name: pkg.name, banner},
-      {file: pkg.module, format: 'es', name: pkg.name, banner},
     ],
     plugins: [
       json({compact: true}),
@@ -27,7 +25,28 @@ export default [
         ],
         exclude: ['node_modules/**'],
       }),
-      nodeResolve(),
+    ],
+    external: ['@hebcal/core'],
+  },
+  {
+    input: 'src/locale.js',
+    output: [
+      {file: pkg.module, format: 'es', name: pkg.name, banner},
+    ],
+    plugins: [
+      json({compact: true, preferConst: true}),
+      babel({
+        babelHelpers: 'bundled',
+        presets: [
+          ['@babel/preset-env', {
+            modules: false,
+            targets: {
+              node: '12.22.0',
+            },
+          }],
+        ],
+        exclude: ['node_modules/**'],
+      }),
     ],
     external: ['@hebcal/core'],
   },
@@ -61,10 +80,8 @@ export default [
           ['@babel/preset-env', {
             modules: false,
             targets: {
-              edge: '17',
-              firefox: '60',
-              chrome: '67',
-              safari: '11.1',
+              'chrome': '58',
+              'ie': '11',
             },
             useBuiltIns: 'usage',
             corejs: 3,
@@ -72,7 +89,6 @@ export default [
         ],
         exclude: ['node_modules/**'],
       }),
-      nodeResolve(),
     ],
     external: ['@hebcal/core'],
   },
