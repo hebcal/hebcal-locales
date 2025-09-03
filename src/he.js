23ebd3a61506a635a2ca0d5af3fe67833181dc89
Michael J. Radwin <michael@radwin.org>
2025-09-02 21:16:27 -0700

    Add he-x-NoNikud also

--- src/he.js
+++ src/he.js
@@ -1,3 +1,16 @@
 import {Locale} from '@hebcal/hdate';
-import po_he from './he.po.js';
-Locale.addTranslations('he', po_he);
+import poHe from './he.po.js';
+
+Locale.addTranslations('he', poHe);
+Locale.addTranslations('h', poHe);
+
+const heStrs = poHe.contexts[''];
+const heNoNikud = {};
+for (const [key, val] of Object.entries(heStrs)) {
+  heNoNikud[key] = [Locale.hebrewStripNikkud(val[0])];
+}
+const poHeNoNikud = {
+  headers: poHe.headers,
+  contexts: {'': heNoNikud},
+};
+Locale.addTranslations('he-x-NoNikud', poHeNoNikud);
